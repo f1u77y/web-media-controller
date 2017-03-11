102f700e464c6f7ea0999dfbab131b0e5a39daa1
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-03-12 02:15:26 +0300

    move utilities to BaseConnector

--- content/connector.js
+++ content/connector.js
@@ -4,6 +4,11 @@
 /* global _ */
 
 class BaseConnector {
+    constructor(properties) {
+        this.setProperties(properties);
+        this.sendMessage({command: 'load'});
+    }
+
     sendMessage(message) {
         chrome.runtime.sendMessage(_(message).pick('command', 'argument'));
     }
@@ -14,13 +19,24 @@ class BaseConnector {
     }
 
     setProperties(props) {
-        chrome.runtime.sendMessage({
-            command: 'set',
-            argument: false,
-        });
+        this.properties = props;
         chrome.runtime.sendMessage({
             command: 'set',
             argument: props,
         });
+    }
+
+    onNewTrack(newTrackInfo) {
+        this.sendMessage({
+            command: 'metadata',
+            argument: newTrackInfo,
+        });
+        this.lastTrackInfo = newTrackInfo;
+    }
+
+    injectScript(url) {
+        const script = document.createElement('script');
+        script.src = chrome.extension.getURL(url);
+        (document.head || document.documentElement).appendChild(script);
     }
 }
