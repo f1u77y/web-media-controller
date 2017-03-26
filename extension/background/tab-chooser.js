2688e03c7ee8eb9494ebe9959529e6056ee7f698
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-03-26 23:52:04 +0300

    Extension: use page action to choose current tab

--- extension/background/tab-chooser.js
+++ extension/background/tab-chooser.js
@@ -55,7 +55,40 @@ define(() => {
         }
 
         changeTab(tabId, sendResponse) {
+            if (this.currentTabId) {
+                chrome.pageAction.hide(this.currentTabId);
+                chrome.pageAction.setTitle({
+                    tabId: this.currentTabId,
+                    title: 'Not playing',
+                });
+                chrome.pageAction.setIcon({
+                    tabId: this.currentTabId,
+                    path: {
+                        '16': 'icons/disconnect-16.png',
+                        '32': 'icons/disconnect-32.png',
+                        '64': 'icons/disconnect-64.png',
+                        '128': 'icons/disconnect-128.png',
+                    },
+                });
+            }
             this.currentTabId = tabId;
+            if (tabId) {
+                chrome.pageAction.show(this.currentTabId);
+                chrome.pageAction.setTitle({
+                    tabId: this.currentTabId,
+                    title: 'Playing',
+                });
+                chrome.pageAction.setIcon({
+                    tabId: this.currentTabId,
+                    path: {
+                        '16': 'icons/playing-16.png',
+                        '32': 'icons/playing-32.png',
+                        '64': 'icons/playing-64.png',
+                        '128': 'icons/playing-128.png',
+                    }
+                });
+            }
+            this.sendMessage({ command: 'reload' });
             if (sendResponse) {
                 sendResponse({status: 'current'});
             }
