5a77a83a2c71ef9e1d928a67c382e5986a30b58b
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-05-21 19:53:24 +0300

    Add post-install messages for reporting errors to user

--- extension/background/main.js
+++ extension/background/main.js
@@ -7,10 +7,45 @@ define([
     './tab-chooser',
 ], (chooser) => {
     const port = chrome.runtime.connectNative('me.f1u77y.web_media_controller');
-    port.onDisconnect.addListener(() => {
-        if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError.message);
+    port.onDisconnect.addListener((p) => {
+        if (p.error) {
+            console.log(p.error.message);
         }
+        chrome.runtime.getPlatformInfo((info) => {
+            if (['linux', 'openbsd'].includes(info.os)) {
+                chrome.notifications.create({
+                    type: 'basic',
+                    iconUrl: chrome.runtime.getURL('icons/disconnect-16.svg'),
+                    title: chrome.i18n.getMessage('native_not_installed_title'),
+                    message: chrome.i18n.getMessage('native_not_installed_message'),
+                    contextMessage: chrome.i18n.getMessage('click_instructions'),
+                    isClickable: true,
+                }, (currentId) => {
+                    chrome.notifications.onClicked.addListener(function instruct(id) {
+                        if (id !== currentId) return;
+                        chrome.notifications.onClicked.removeListener(instruct);
+                        chrome.tabs.create({
+                            url: chrome.i18n.getMessage('native_instructions_url'),
+                        });
+                    });
+                });
+            } else {
+                chrome.notifications.create({
+                    type: 'basic',
+                    iconUrl: chrome.runtime.getURL('icons/disconnect-16.svg'),
+                    title: chrome.i18n.getMessage('unsupported_os_title'),
+                    message: chrome.i18n.getMessage('unsupported_os_message'),
+                    contextMessage: chrome.i18n.getMessage('click_uninstall'),
+                    isClickable: true,
+                }, (currentId) => {
+                    chrome.notifications.onClicked.addListener(function uninstall(id) {
+                        if (id !== currentId) return;
+                        chrome.notifications.onClicked.removeListener(uninstall);
+                        chrome.management.uninstallSelf();
+                    });
+                });
+            }
+        });
     });
     port.onMessage.addListener((message) => {
         chooser.sendMessage(_.defaults(message, { argument: null }));
