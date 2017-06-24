1e27703755e6c08f6ae95690b0c0145782d9d4e4
alexesprit <alex.esprit@gmail.com>
2017-06-24 11:37:54 +0300

    Add support for Rainmeter WebNowPlaying plugin
    Add new object called 'adapter', which gets messages from content
    scipts, transforms them (if required) and sends these messages to remote
    applications.

--- background/tab-chooser.js
+++ background/tab-chooser.js
@@ -2,28 +2,9 @@
 
 define([
     'background/utils',
+    'background/listener-manager',
     'common/prefs',
-], (Utils, prefs) => {
-    class ListenerManager {
-        constructor() {
-            this.listeners = new Set();
-        }
-
-        addListener(listener) {
-            this.listeners.add(listener);
-        }
-
-        removeListener(listener) {
-            this.listeners.remove(listener);
-        }
-
-        call(...args) {
-            for (let listener of this.listeners) {
-                listener(...args);
-            }
-        }
-    }
-
+], (Utils, ListenerManager, prefs) => {
     class TabChooser {
         constructor() {
             this.ports = new Map();
