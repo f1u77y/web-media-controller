4ab5868f602e2779d419955812cb8d165e160340
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-06-30 10:14:57 +0300

    Fix ESLint warning

--- options/i18n.js
+++ options/i18n.js
@@ -4,7 +4,7 @@ define(() => {
     const iter = document.createNodeIterator(document.documentElement,
                                              NodeFilter.SHOW_ELEMENT);
     let node = null;
-    while (node = iter.nextNode()) {
+    while ((node = iter.nextNode())) {
         if (node.hasAttribute('i18n-text')) {
             const id = node.getAttribute('i18n-text');
             node.textContent = chrome.i18n.getMessage(id);
