6db0d047f4f2d149c58f70f1f23019b294c5144a
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-07-01 18:11:08 +0300

    Simplify API for injected scripts
    - Use class properties instead of addGetter/addListener
    - Separate setters from listeners

--- inject/deezer.js
+++ inject/deezer.js
@@ -1,21 +1,21 @@
 'use strict';
 
-(new class extends PageHelper {
-    start() {
+new class extends PageHelper {
+    constructor() {
+        super();
+
         if (!this.canStart()) return;
-
-        this.addListener('set currentTime', ({ position, length }) => {
-            window.dzPlayer.control.seek(position / length);
-        });
-        this.addListener('seek', ({ offset, position, length }) => {
-            window.dzPlayer.control.seek((position + offset) / length);
-        });
-        this.addListener('set volume', (volume) => {
-            window.dzPlayer.control.setVolume(volume);
-        });
-
-        this.addGetter('album', () => window.dzPlayer.getAlbumTitle());
-        this.addGetter('uniqueId', () => window.dzPlayer.getSongId());
-        this.addGetter('canSeek', () => window.dzPlayer.control.canSeek());
     }
-}).start();
+
+    set currentTime({ position, length }) {
+        window.dzPlayer.control.seek(position / length);
+    }
+
+    seek({ offset, position, length }) {
+        window.dzPlayer.control.seek((position + offset) / length);
+    }
+
+    set volume(volume) {
+        window.dzPlayer.control.setVolume(volume);
+    }
+};
--- inject/vk.js
+++ inject/vk.js
@@ -1,10 +1,23 @@
 'use strict';
 
-(new class extends PageHelper {
+new class extends PageHelper {
     constructor() {
         super();
+
+        if (!this.canStart()) return;
+
         this.e = 35;
         this.playbackStatus = 'stopped';
+
+        this.changeProperties(['volume', 'playbackStatus']);
+
+        for (let event of ['start', 'pause', 'stop', 'volume', 'progress']) {
+            const ev = event;
+            window.ap.subscribers.push({
+                et: ev,
+                cb: () => this.handlePlayerEvent(ev),
+            });
+        }
     }
 
     logVolume(num) {
@@ -15,44 +28,8 @@
         return Math.log(1 + num * (this.e - 1)) / Math.log(this.e);
     }
 
-    set volume(volume) {
-        window.ap.setVolume(this.logVolume(volume));
-    }
-
-    get volume() {
-        return this.unlogVolume(window.ap.getVolume());
-    }
-
     get audioElement() {
         return window.ap._impl._currentAudioEl || {};
-    }
-
-    get trackInfo() {
-        function last(array) {
-            return array[array.length - 1];
-        }
-
-        const infoIndex = {
-            artist: 4,
-            title: 3,
-            length: 5,
-            artUrl: 14,
-        };
-        const audioObject = window.ap._currentAudio;
-        let trackInfo = {
-            artist: audioObject[infoIndex.artist],
-            title: audioObject[infoIndex.title],
-            length: audioObject[infoIndex.length] * 1000,
-        };
-        if (audioObject[infoIndex.artUrl]) {
-            trackInfo.artUrl = last(audioObject[infoIndex.artUrl].split(','));
-        }
-        return trackInfo;
-    }
-
-    get currentTime() {
-        let {currentTime} = this.audioElement;
-        return (currentTime || 0) * 1000;
     }
 
     handlePlayerEvent(event) {
@@ -83,39 +60,67 @@
         this.changeProperties(Object.keys(scope).filter(name => scope[name] != null));
     }
 
-    start() {
-        if (!this.canStart()) return;
+    play() { window.ap.play(); }
 
-        this.addListener('play', () => window.ap.play());
-        this.addListener('pause', () => window.ap.pause());
-        this.addListener('playPause', () => {
-            if (window.ap.isPlaying()) {
-                window.ap.pause();
-            } else {
-                window.ap.play();
-            }
-        });
-        this.addListener('stop', () => window.ap.stop());
-        this.addListener('next', () => window.ap.playNext());
-        this.addListener('previous', () => window.ap.playPrev());
-        this.addListener('seek', offset => this.audioElement.currentTime += offset/1000);
-        this.addListener('set position', pos => this.audioElement.currentTime = pos/1000);
-        this.addListener('set volume', volume => this.volume = volume);
+    pause() { window.ap.pause(); }
 
-        this.addGetter('playbackStatus', () => this.playbackStatus);
-        this.addGetter('volume', () => this.volume);
-        this.addGetter('trackInfo', () => this.trackInfo);
-        this.addGetter('currentTime', () => this.currentTime);
-        this.addGetter('uniqueId', () => window.ap.getCurrentAudio()[0]);
-
-        this.changeProperties(['volume', 'playbackStatus']);
-
-        for (let event of ['start', 'pause', 'stop', 'volume', 'progress']) {
-            const ev = event;
-            window.ap.subscribers.push({
-                et: ev,
-                cb: () => this.handlePlayerEvent(ev),
-            });
+    playPause() {
+        if (window.ap.isPlaying()) {
+            window.ap.pause();
+        } else {
+            window.ap.play();
         }
     }
-}).start();
+
+    stop() { window.ap.stop(); }
+
+    next() { window.ap.playNext(); }
+
+    previous() { window.ap.playPrev(); }
+
+    seek(offset) { this.audioElement.currentTime += offset / 1000; }
+
+    set currentTime(currentTime) {
+        this.audioElement.currentTime = currentTime / 1000;
+    }
+
+    set volume(volume) {
+        window.ap.setVolume(this.logVolume(volume));
+    }
+
+    get volume() {
+        return this.unlogVolume(window.ap.getVolume());
+    }
+
+    get uniqueId() {
+        return window.ap.getCurrentAudio()[0];
+    }
+
+    get trackInfo() {
+        function last(array) {
+            return array[array.length - 1];
+        }
+
+        const infoIndex = {
+            artist: 4,
+            title: 3,
+            length: 5,
+            artUrl: 14,
+        };
+        const audioObject = window.ap._currentAudio;
+        let trackInfo = {
+            artist: audioObject[infoIndex.artist],
+            title: audioObject[infoIndex.title],
+            length: audioObject[infoIndex.length] * 1000,
+        };
+        if (audioObject[infoIndex.artUrl]) {
+            trackInfo.artUrl = last(audioObject[infoIndex.artUrl].split(','));
+        }
+        return trackInfo;
+    }
+
+    get currentTime() {
+        let { currentTime } = this.audioElement;
+        return (currentTime || 0) * 1000;
+    }
+};
