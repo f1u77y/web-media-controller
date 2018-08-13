d5bc78b28e9780743b09c3083f02052d6963ae07
Bogdan Sinitsyn <f1u77y@yandex.ru>
2018-08-13 17:15:20 +0300

    Add import to background/utils

--- background/utils.js
+++ background/utils.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import browser from 'webextension-polyfill';
+
 function makeIconPath(name, sizes = [16, 32, 64, 128], extension = 'png') {
     let result = {};
     for (let size of sizes) {
