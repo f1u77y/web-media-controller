5e854a12849d931e46864b59828bc49a73469d14
Tim Brooks <brooks@skoorb.net>
2019-08-25 22:09:09 +0100

    Implement style guide changes and append to README

--- src/connectors/amazon-music.js
+++ src/connectors/amazon-music.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import BaseConnector from 'content/base-connector';
 import Utils from 'content/utils';
 
@@ -18,39 +16,37 @@ new class extends BaseConnector {
         this.prevButtonSelector = 'span.playbackControls span.previousButton';
         this.nextButtonSelector = 'span.playbackControls span.nextButton';
 
-        this.length_last = 0;
-        this.length_title = '';
+        this.lengthLast = 0;
+        this.lengthTitle = '';
 
-        Utils.query('.playbackControlsView').then(elem => this.observe(elem));
+        Utils.query('.playbackControlsView').then((elem) => this.observe(elem));
     }
 
     get playbackStatus() {
-        return Utils.query('span.playButton').then(elem => {
-            return elem.classList.contains("playerIconPause") ? 'playing' : 'paused';
-        });
+        return Utils.query('span.playButton').then((elem) => (elem.classList.contains('playerIconPause') ? 'playing' : 'paused'));
     }
 
     get length() {
-        var title = document.querySelector(this.titleSelector).innerText;
-        var remaining = document.querySelector(".listViewDuration").innerText.split(':');
-        var remaining_ms = -parseInt(remaining[0]) * 60000 + parseInt(remaining[1]) * 1000;
-        var progress = 0.01 * parseFloat(document.querySelector("div.scrubberHandle")['style']['left']);
-        var length = Math.floor(remaining_ms / (1 - progress));
-        var diff = Math.abs(length - this.length_last);
-        if (title != this.length_title || diff > 2000 && progress < 0.5) {
-            if (length == 0 || isNaN(length)) return undefined;
-            this.length_last = length;
-            this.length_title = title;
+        const title = document.querySelector(this.titleSelector).innerText;
+        let remaining = document.querySelector('.listViewDuration').innerText.split(':');
+        remaining = -parseInt(remaining[0]) * 60000 + parseInt(remaining[1]) * 1000;
+        const progress = 0.01 * parseFloat(document.querySelector('div.scrubberHandle')['style']['left']);
+        const length = Math.floor(remaining / (1 - progress));
+        const diff = Math.abs(length - this.lengthLast);
+        if (title !== this.lengthTitle || diff > 2000 && progress < 0.5) {
+            if (length === 0 || isNaN(length)) return undefined;
+            this.lengthLast = length;
+            this.lengthTitle = title;
             return length;
         } else {
-            return this.length_last;
+            return this.lengthLast;
         }
     }
 
     get currentTime() {
-        var remaining = document.querySelector(".listViewDuration").innerText.split(':');
-        if (remaining[0] == "") return undefined;
-        var remaining_ms = -parseInt(remaining[0]) * 60000 + parseInt(remaining[1]) * 1000;
-        return Math.floor(this.length - remaining_ms);
+        let remaining = document.querySelector('.listViewDuration').innerText.split(':');
+        if (remaining[0] === '') return undefined;
+        remaining = -parseInt(remaining[0]) * 60000 + parseInt(remaining[1]) * 1000;
+        return Math.floor(this.lengthLast - remaining);
     }
-};
+}();
